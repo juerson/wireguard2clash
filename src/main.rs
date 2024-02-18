84be7f7c549fce837f6f7ba2213b8071baf34b33
unknown <zhangrson@163.com>
2024-02-18 23:19:14 +0800

    适配clash-verge-rev的mihomo meta内核

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,14 @@ use std::{
 };
 
 const BASIC_INFO: &str = r#"mode: rule
-socks-port: 7891
 port: 7890
+socks-port: 7891
 allow-lan: false
 log-level: info
 secret: ''
+unified-delay: true
 external-controller: 127.0.0.1:9097
+global-client-fingerprint: chrome
 dns:
   enable: true
   listen: :53
@@ -36,8 +38,6 @@ dns:
     geoip-code: CN
     ipcidr:
     - 240.0.0.0/4
-unified-delay: true
-global-client-fingerprint: chrome
 proxies:"#;
 const PROXY_GROUPS1: &str = r#"proxy-groups:
   - name: 节点选择
@@ -474,13 +474,13 @@ fn main() -> io::Result<()> {
         "162.159.193.0/24",
         "162.159.195.0/24",
     ];
-    let ports = vec![2408];
-    // let ports = vec![
-    //     500, 854, 859, 864, 878, 880, 890, 891, 894, 903, 908, 928, 934, 939, 942, 943, 945, 946,
-    //     955, 968, 987, 988, 1002, 1010, 1014, 1018, 1070, 1074, 1180, 1387, 1701, 1843, 2371, 2408,
-    //     2506, 3138, 3476, 3581, 3854, 4177, 4198, 4233, 4500, 5279, 5956, 7103, 7152, 7156, 7281,
-    //     7559, 8319, 8742, 8854, 8886,
-    // ];
+    // let ports = vec![2408];
+    let ports = vec![
+        500, 854, 859, 864, 878, 880, 890, 891, 894, 903, 908, 928, 934, 939, 942, 943, 945, 946,
+        955, 968, 987, 988, 1002, 1010, 1014, 1018, 1070, 1074, 1180, 1387, 1701, 1843, 2371, 2408,
+        2506, 3138, 3476, 3581, 3854, 4177, 4198, 4233, 4500, 5279, 5956, 7103, 7152, 7156, 7281,
+        7559, 8319, 8742, 8854, 8886,
+    ];
 
     // 定义一个计数器来计算content写入的个数(也就是proxies中最多写多少个节点)
     let mut content_count = 0;
